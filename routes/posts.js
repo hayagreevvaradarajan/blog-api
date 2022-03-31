13d0af78073b07184d0ccb2fabaaae9dd909c20a
Hayagreev <hayagreev.chintu1@gmail.com>
2022-03-31 13:07:43 +0530

    added code to make a single axios request and return that response in get /api.posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,18 @@
-const posts = (req, res) => {
+const axios = require("axios");
+const postsObject = {};
+const postsArray = [];
+const sendRequest = async (tags) => {
+    const response = await axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tags}`);
+    for(let i=0; i<response.data.posts.length;i++){
+        postsObject[response.data.posts[i].id] = response.data.posts[i];
+    }
+    return postsObject;
+};
+
+const posts = async (req, res) => {
+    const response = await sendRequest("tech");
     res.status(200).json({
-        "message": "get request to /posts"
+        "posts":response
     });
 };
 
